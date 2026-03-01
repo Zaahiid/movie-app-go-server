3f5b6acf14e6f2b68d397d479a62e243253c2caa
Zaahiid <zahidrsheikh01@gmail.com>
2026-03-01 23:25:07 +0530

    feat: Implement initial Gin web server with a `/hello` endpoint.

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// Step 2: Basic Server
+	router := gin.Default()
+
+	// Define a simple /hello route
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies!")
+	})
+
+	// Start the server on port 8080
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+}
